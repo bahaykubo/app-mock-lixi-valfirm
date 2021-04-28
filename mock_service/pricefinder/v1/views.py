7ed085aecdacce8363f52480cf1e0c5a19075f95
Noel Sevilla <Noel.Sevilla@valocityglobal.com>
2021-04-29 10:37:37 +1200

    add pricefinder image test and sample http request

--- mock_service/pricefinder/v1/views.py
+++ mock_service/pricefinder/v1/views.py
@@ -10,9 +10,13 @@ import re
 @csrf_exempt
 @require_http_methods(['GET'])
 def images(request, image_id):
-    random_image = None
-    if not re.search('^[9][0][0-7]$', str(image_id)):
-        random_image = random.randint(900, 907)
+    random_image = _image_id_selector(image_id)
     with open(f'./mock_service/pricefinder/files/images/{random_image if random_image else image_id}.jpg', "rb") as f:
         return HttpResponse(f.read(), content_type="image/jpeg")
 
+
+def _image_id_selector(image_id):
+    if not re.search('^[9][0][0-7]$', str(image_id)):
+        return random.randint(900, 907)
+    else:
+        return None
