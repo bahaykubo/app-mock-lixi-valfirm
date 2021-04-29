1d51cf502842c540d6aea35e3f3b47fa8202eed0
Noel Sevilla <Noel.Sevilla@valocityglobal.com>
2021-04-29 22:47:23 +1200

    add reference to pricefinder api

--- mock_service/pricefinder/urls.py
+++ mock_service/pricefinder/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, re_path, include
 
+# see open api spec for pricefinder endpoints we are mocking here
+# https://api.pricefinder.com.au/v1/swagger/index.html#/
+
 urlpatterns = [
     path('v1/', include('mock_service.pricefinder.v1.urls'))
 ]
