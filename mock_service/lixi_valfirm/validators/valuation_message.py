031d09256fdc1ea654612901b785aac48cd25506
noelsevilla <noel.sev@gmail.com>
2026-06-23 11:31:46 +1200

    Update python to later version and swap spyne with plain django views

--- mock_service/lixi_valfirm/validators/valuation_message.py
+++ mock_service/lixi_valfirm/validators/valuation_message.py
@@ -4,7 +4,10 @@ from lxml import etree
 def valid_message(xml_string, schema):
     schema_doc = etree.parse(schema)
     schema = etree.XMLSchema(schema_doc)
-    xml = etree.XML(xml_string)
+    try:
+        xml = etree.XML(xml_string)
+    except TypeError as e:
+        raise ValueError(str(e)) from e
     result = schema.validate(xml)
     return result
 
